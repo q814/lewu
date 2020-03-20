4db743cec91d8c118a9dd2bc2ff85d18dd2dfe16
yuyuan <895868773@qq.com>
2020-03-20 19:03:34 +0800

    “第一次提交”

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <keep-alive include="broad">
+    <router-view/>
+    <tabber :ishow="bol"></tabber>
+  </div>
+</template>
+
+<script>
+import Tabbar from '../src/components/tabber' 
+import Jquery from 'jquery'
+export default {
+  name: 'App',
+  components:{
+    Tabbar
+  },
+
+  data() {
+    return {
+      bol:true
+    }
+
+  },
+
+  watch:{
+    '$route':function(to,from){
+      let arr=['/home','/mine']
+      if(arr.includes(to.path)){
+        this.bol=true
+      }else{
+        this.bol=false
+      }
+    }
+  },
+
+
+
+
+
+
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/tabber.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <router-link to="meirong">美容护肤</router-link>
+    <router-link to="muying">母婴儿童</router-link>
+    <router-link to="xiangbao">箱包配饰</router-link>
+    <router-link to="meirong">美容器具</router-link>
+    <router-link to="jiajv">家居生活</router-link>
+  </div>
+</template>
